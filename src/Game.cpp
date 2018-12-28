b39b4452533179fa125e8016bae314a8d1f141b9
Yumi Ta <itsyumietanaka@gmail.com>
2018-12-27 20:58:57 -0500

    Add documentation for versioning.

--- src/Game.cpp
+++ src/Game.cpp
@@ -6,6 +6,11 @@ namespace Game{
 	bool not_over = true;
 	
 	void game_loop(){
+		// Start incrementing only after first working, playable build
+		// SYNTAX: Major.Minor.Patch[a/b/nothing]
+		// Major - Overhauls, non-backwards-compatibility, etc
+		// Minor - Feature additions
+		// Patch - Bug fixes
 		std::cout << "Welcome to Ember v0.0.1a!\n";
 		
 		while (not_over){
