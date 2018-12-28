b6e090af273344d03dc59c63e43203826cc6ca7e
Yumi Ta <itsyumietanaka@gmail.com>
2018-12-27 20:40:43 -0500

    Add welcome message with version.

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,9 +1,13 @@
 #include "Game.hpp"
 
+#include <iostream>
+
 namespace Game{
 	bool not_over = true;
 	
 	void game_loop(){
+		std::cout << "Welcome to Ember v0.0.1a!\n";
+		
 		while (not_over){
 		
 		}
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
 #include <random>
 #include <ctime>
 
 #include "Game.hpp"
 
 void setup(){
-	std::ios_base::sync_with_stdio(false);
 	srand(time(nullptr));
 }
 
